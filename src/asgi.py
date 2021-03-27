4443544ec30f83b45ad52733b0270bd001b9947a
Dawid Mszanowski <dmszanowski@icloud.com>
2021-03-28 00:18:02 +0100

    Update project name

--- src/asgi.py
+++ src/asgi.py
@@ -6,7 +6,7 @@ from django.core.asgi import get_asgi_application
 
 import chat.routing
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chatproject.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "src.settings")
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
