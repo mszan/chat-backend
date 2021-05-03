18c8fa372f2a39a59552dd09bbeca846bbc43985
Dawid Mszanowski <dmszanowski@icloud.com>
2021-05-03 16:58:45 +0200

    add new host
    due to mszan/chat-frontend@60e26c30bfb310386517142f9740eafaec852fd0

--- src/settings/development.py
+++ src/settings/development.py
@@ -10,7 +10,7 @@ SECRET_KEY = "not_secret"
 
 DEBUG = 1
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0", "dev-chat.mszanowski.pl"]
 
 DATABASES = {
     'default': {
