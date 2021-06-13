dfa3f415f9125d9b90c20f561b9f0b5fd596037f
Dawid Mszanowski <dmszanowski@icloud.com>
2021-06-13 18:57:38 +0200

    update production settings

--- src/settings/production.py
+++ src/settings/production.py
@@ -1,9 +1,26 @@
 # Part of settings for production environment.
 
 from .base import *
+import os
 
-SECRET_KEY = "not_secret"
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
+# TODO: Make this false.
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', 'localhost', 'chat.mszanowski.pl']
+ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', 'localhost', 'chat-app.mszanowski.pl']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': os.environ['DJANGO_DB_PASSWORD'],
+        'HOST': 'postgres',
+        'PORT': 5432,
+    }
+}
+
+CORS_ALLOWED_ORIGINS = [
+    "https://chat-app.mszanowski.pl"
+]
